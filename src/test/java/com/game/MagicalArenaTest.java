37d36425c006ec97ae1e84e1bddc09fbb9c9541c
mayankksinha1 <mayankksinha1@gmail.com>
2024-02-18 01:05:00 +0530

    [Magical Arena] : Junit test case for null health of either of the player.

--- src/test/java/com/game/MagicalArenaTest.java
+++ src/test/java/com/game/MagicalArenaTest.java
@@ -7,12 +7,28 @@ public class MagicalArenaTest {
 
 
     @Test
-    public void testGame(){
+    public void testGameBothPlayerZeroHealth(){
         Player player1 = new Player(1, "Player1", 0, 5, 10);
         Player player2 = new Player(2, "Player2", 0, 10, 5);
 
         MagicalArena arena = new MagicalArena(player1, player2);
         arena.playMatch();
         Assert.assertEquals(arena.getWinner(), null);
+    }
+
+    @Test
+    public void testGameEitherPlayerZeroHealth(){
+        Player player1 = new Player(1, "Player1", 0, 5, 10);
+        Player player2 = new Player(2, "Player2", 10, 10, 5);
+
+        MagicalArena arena = new MagicalArena(player1, player2);
+        arena.playMatch();
+        Assert.assertEquals(arena.getWinner(), player2);
+
+        player1.setHealth(10);
+        player2.setHealth(0);
+        arena = new MagicalArena(player1, player2);
+        arena.playMatch();
+        Assert.assertEquals(arena.getWinner(), player1);
     }
 }
